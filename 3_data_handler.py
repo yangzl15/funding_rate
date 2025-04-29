64d57b2c706dd991720c0d23a3092e5888d22183
yangzl15 <14996159+yangzl15@user.noreply.gitee.com>
2025-04-29 17:38:08 +0800

    first commit

--- 3_data_handler.py
+++ 3_data_handler.py
@@ -38,7 +38,9 @@ def process_csv_file(csv_path, start_date, end_date):
     series = df["funding rate"].reindex(full_index)
     return series
 
-def combine_all_symbols(csv_dir, start_date, end_date, start_date_override=None):
+
+# --- MOD ---
+def combine_all_symbols(csv_dir, start_date, end_date, start_date_override=None, start_label_override=None):
     file_list = [os.path.join(csv_dir, f) for f in os.listdir(csv_dir) if f.endswith('.csv')]
     print(f"len(file_list): {len(file_list)}")
     combined_df = None
@@ -48,8 +50,16 @@ def combine_all_symbols(csv_dir, start_date, end_date, start_date_override=None)
         if parts:
             symbol = parts
             series = process_csv_file(fp, start_date, end_date)
-            if start_date_override is not None:
-                # 只保留大于 start_date_override 的部分
+            if start_date_override is not None and start_label_override is not None: ## 开始5min增量
+                mask = (
+                    (series.index.get_level_values("date") > pd.to_datetime(start_date_override)) |
+                    (
+                        (series.index.get_level_values("date") == pd.to_datetime(start_date_override)) &
+                        (series.index.get_level_values("Label") > start_label_override)
+                    )
+                )
+                series = series.loc[mask]
+            elif start_date_override is not None: #第一次跑历史+增量
                 series = series.loc[series.index.get_level_values("date") >= pd.to_datetime(start_date_override)]
             if combined_df is None:
                 combined_df = pd.DataFrame({symbol: series})
@@ -58,6 +68,8 @@ def combine_all_symbols(csv_dir, start_date, end_date, start_date_override=None)
     combined_df.columns = combined_df.columns.str.lower()
     logging.info(f"processed and combined to 5min data but unfilled")
     return combined_df
+# --- /MOD ---
+
 
 def process_symbol(args):
     symbol, s_orig, dates = args
@@ -127,10 +139,10 @@ def process_funding_rate(input_df, configurations):
     logging.info("All funding rate frequencies processed successfully.")
     return output_dfs
 
-def save_all_results(df_target_history, df_target_rt, raw_df, df_target, output_dfs, debug_data_dir, output_dir):
+def save_all_results(df_target_history, df_target_rt, df_target, output_dfs, debug_data_dir, output_dir):
     df_target_history.to_parquet(os.path.join(debug_data_dir, "funding_5min_history.parquet"))
     df_target_rt.to_parquet(os.path.join(debug_data_dir, "funding_5min_realtime.parquet"))
-    raw_df.to_parquet(os.path.join(debug_data_dir, "funding_5min_history_unfilled.parquet"))
+    # raw_df.to_parquet(os.path.join(debug_data_dir, "funding_5min_history_unfilled.parquet"))
     df_target.to_parquet(os.path.join(debug_data_dir, "funding_5min_combined.parquet"))
 
     for interval, df_resampled in output_dfs.items():
@@ -146,7 +158,7 @@ if __name__ == "__main__":
     os.makedirs(config.debug_data_dir, exist_ok=True)
 
     first_run = True
-    if first_run == True:
+    if first_run == True: ## 第一次跑历史+增量
         history_start_date = config.start_date
         history_end_date = (pd.to_datetime(config.end_date) - pd.Timedelta(days=2)).strftime("%Y-%m-%d")
         rt_start_date = (pd.to_datetime(config.end_date) - pd.Timedelta(days=1)).strftime("%Y-%m-%d")
@@ -160,7 +172,8 @@ if __name__ == "__main__":
             config.csv_history_dir,
             history_start_date,
             history_end_date,
-            start_date_override = None
+            start_date_override = None,
+            start_label_override=None
         )
         df_target_history = fill_target_from_orig_parallel(raw_df, config.start_date, history_end_date)
 
@@ -171,20 +184,34 @@ if __name__ == "__main__":
             config.csv_realtime_dir,
             rt_start_date,
             rt_end_date,
-            start_date_override= last_date_in_history
+            start_date_override= last_date_in_history,
+            start_label_override=None
         )
 
         # columns取并集
         df_target = pd.concat([df_target_history, df_target_rt], axis=0, ignore_index=False)
-        # drop any rows with duplicate index
-        # df_target = df_target.loc[~df_target.index.duplicated(keep='first')]
         output_dfs = process_funding_rate(df_target, config.configurations)
 
-        save_all_results(df_target_history, df_target_rt, raw_df, df_target, output_dfs, config.debug_data_dir, config.output_dir)
+        save_all_results(df_target_history, df_target_rt, df_target, output_dfs, config.debug_data_dir, config.output_dir)
 
-    else: 
-        # 读取上次处理的结果
-        df_target = pd.read_parquet(os.path.join(config.debug_data_dir, "funding_5min_combined.parquet"))
-        last_date_in_history, last_label_in_history = df_target.index.get_level_values("date").max(), df_target.index.get_level_values("Label").max()
-        print(f"last_date_in_history: {last_date_in_history}, last_label: {last_label_in_history}")
+    else: ## 增量更新
+        df_target = pd.read_parquet(os.path.join(config.debug_data_dir, "funding_5min_combined_test.parquet"))
+        df_target = df_target.dropna(how='all')
+        last_date = df_target.index.get_level_values("date").max()  # --- MOD ---
+        last_label = df_target.loc[last_date].index.get_level_values("Label").max()  # --- MOD ---
+        rt_start_date = last_date.strftime("%Y-%m-%d")  # --- MOD ---
+        rt_end_date = config.end_date  # --- MOD ---
 
+        df_target_addition = combine_all_symbols(  # --- MOD ---
+            config.csv_realtime_dir,
+            rt_start_date,
+            rt_end_date,
+            start_date_override=last_date,
+            start_label_override=last_label
+        )
+
+        df_target_new = pd.concat([df_target, df_target_addition], axis=0, ignore_index=False)
+
+        output_dfs = process_funding_rate(df_target_new, config.configurations)  # --- MOD ---
+
+        save_all_results(df_target, df_target_addition, df_target_new, output_dfs, config.debug_data_dir, config.output_dir)  # --- MOD ---
